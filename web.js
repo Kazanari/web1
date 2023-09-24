229113f12d8323093f095ae39a2d5435396b8327
Kazanari <501365140@qq.com>
2023-09-24 18:51:59 +0900

    003

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 function calculate() {
-    let typeCost = parseInt(document.getElementById('type').value);
-    let domainCost = parseInt(document.getElementById('domain').value);
-    let serverCost = parseInt(document.getElementById('server').value);
-    let pageCountCost = parseInt(document.getElementById('pageCount').value) * 1000; // 假设每页1000円
-    let photosCost = parseInt(document.getElementById('photos').value);
-    let contentPlanCost = parseInt(document.getElementById('contentPlan').value);
-    let cmsCost = parseInt(document.getElementById('cms').value);
-    let smartphoneCost = parseInt(document.getElementById('smartphone').value);
-    let seoCost = parseInt(document.getElementById('seo').value);
-    let publishTimeCost = parseInt(document.getElementById('publishTime').value);
+    let typeCost = parseInt(document.getElementById('type').value) || 0;
+    let domainCost = parseInt(document.getElementById('domain').value) || 0;
+    let serverCost = parseInt(document.getElementById('server').value) || 0;
+    let pageCountCost = parseInt(document.getElementById('pageInput').value) * 1000 || 0; // 假设每页1000円
+    let photosCost = parseInt(document.getElementById('photos').value) || 0;
+    let contentPlanCost = parseInt(document.getElementById('contentPlan').value) || 0;
+    let cmsCost = parseInt(document.getElementById('cms').value) || 0;
+    let smartphoneCost = parseInt(document.getElementById('smartphone').value) || 0;
+    let seoCost = parseInt(document.getElementById('seo').value) || 0;
+    let publishTimeCost = parseInt(document.getElementById('publishTime').value) || 0;
 
     let totalCost = typeCost + domainCost + serverCost + pageCountCost + photosCost + contentPlanCost + cmsCost + smartphoneCost + seoCost + publishTimeCost;
     document.querySelector('#result span').textContent = totalCost;
